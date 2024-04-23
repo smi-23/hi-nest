94cdbbf8c05e46eac79d874fa073c4addae73178
smi-23 <latack789@naver.com>
2024-04-23 09:26:06 +0900

    Refactor: Implement dependency injection in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,14 +5,16 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // nest는 컨트롤러와 비즈니스 로직을 분리하고 싶어함
+  // controller는 그저 url을 가져오는 역할 service는 비즈니스 로직을 담당함
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
   // npm install @nestjs/common으로 자동 임포트
-  @Post('/hello')
+  @Get('/hello')
   sayHello(): string {
-    return 'Hello everyone'
+    return this.appService.getHi();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World jogaram test!';
   }
+
+  getHi(): string {
+    return 'hi nest from service';
+  }
 }
