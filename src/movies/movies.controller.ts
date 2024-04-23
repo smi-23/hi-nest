1c3e7a7179b800a86e987131ee8db0dc200d58fb
smi-23 <latack789@naver.com>
2024-04-23 10:20:54 +0900

    Feature: url이 다른 함수의 파라미터로 인식되지 않게 하기

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies') // entry point
 export class MoviesController {
@@ -8,14 +8,21 @@ export class MoviesController {
         return 'This will return all movies';
     }
 
+    // search를 아래의 getone함수에서 id로 인식하게 하지 않기 위해서 위로 올려줌 
+    @Get('search')
+    search(@Query('year') searchingYear: string){
+        return `we are searching for a movie made after: ${searchingYear}`;
+    }
+
     @Get('/:id')
     getOne(@Param('id') movieId: string) {
         return `This will return one movie with the id: ${movieId}`;
     }
 
     @Post()
-    create() {
-        return 'This will create a movie';
+    create(@Body() movieData) {
+        // console.log(movieData);
+        return movieData;
     }
 
     @Delete('/:id')
@@ -25,9 +32,20 @@ export class MoviesController {
 
     // 리소스의 일부분만 업데이트할 경우 patch 전체를 업데이트할 경우 put
     @Patch('/:id')
-    patch(@Param('id') movieId: string){
-        return `This will patch a movie with the id: ${movieId}`;
+    patch(@Param('id') movieId: string, @Body() updateData) {
+        return {
+            updateData: movieId,
+            ...updateData
+        };
     }
+
+    // url에 /가 필요없을 수 있음
+    // 결과값을 보면 This will return one movie with the id: search라고 나오는데 search를 위 함수처럼 id라고 인식 중
+    // @Get('search')
+    // search(){
+    //     return `we are searching for a movie with a title: `;
+    // }
+
 
 }
 // spec은 테스트파일인데 지우고 진행 중
