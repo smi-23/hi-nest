d311bc8152719bbbf9f7683abb41c3b0ba5fb470
smi-23 <latack789@naver.com>
2024-04-23 09:53:52 +0900

    feature: Delete, Patch in controller

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('movies') // entry point
 export class MoviesController {
@@ -9,9 +9,25 @@ export class MoviesController {
     }
 
     @Get('/:id')
-    getOne(@Param('id') movieId: string){
+    getOne(@Param('id') movieId: string) {
         return `This will return one movie with the id: ${movieId}`;
-    } 
+    }
+
+    @Post()
+    create() {
+        return 'This will create a movie';
+    }
+
+    @Delete('/:id')
+    remove(@Param('id') movieId: string) {
+        return `this will delete a movie with the id: ${movieId}`;
+    }
+
+    // 리소스의 일부분만 업데이트할 경우 patch 전체를 업데이트할 경우 put
+    @Patch('/:id')
+    patch(@Param('id') movieId: string){
+        return `This will patch a movie with the id: ${movieId}`;
+    }
 
 }
 // spec은 테스트파일인데 지우고 진행 중
